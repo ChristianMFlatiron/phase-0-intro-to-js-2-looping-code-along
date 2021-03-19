461ddb099c09eb838a77e2e79c7a0a30efd6abe4
ChristianMFlatiron <chrisrmonteverde@gmail.com>
2021-03-18 23:09:11 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+const newArray = []
+
+function writeCards(cards, event) {
+  for (let i = 0; i < cards.length; i++) {
+    //console.log(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+     newArray.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+  }
+  return newArray
+}
+ 
+
+
+
+
+
+function countDown(time){
+    while (time >= 0){
+        console.log(time--);
+    }
+}
+
